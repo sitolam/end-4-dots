bea5c50118201cb4dcdffeef27c4e11dd395f060
sitolam <79202140+sitolam@users.noreply.github.com>
2025-04-13 00:26:05 +0600

    fix: remove fuzzle selection color conflicts stylix

--- modules/fuzzle.nix
+++ modules/fuzzle.nix
@@ -17,7 +17,7 @@ in
         colors = {
           # background = "1D1011ff";
           text = "F7DCDEff";
-          selection = "574144ff";
+          # selection = "574144ff";
           selection-text = "DEBFC2ff";
           # border = "574144dd";
           # match = "FFB2BCff";
