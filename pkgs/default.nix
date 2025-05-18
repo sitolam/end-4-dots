c460a32ae80c1d40306d7389e2c4bc7751dc840e
sitolam <otis.lammertyn@gmail.com>
2025-05-18 10:22:12 +0200

    Revert "fix"
    This reverts commit cdcd42f1dd4f001062e1250f4e24fcca1ee3a11a.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,7 +9,6 @@ lib.fix(self: {
   illogical-impulse-ags = pkgs.callPackage ./illogical-impulse-ags {};
   illogical-impulse-ags-launcher = pkgs.callPackage ./illogical-impulse-ags-launcher { inherit ags; };
   illogical-impulse-hyprland-shaders = pkgs.callPackage ./illogical-impulse-hyprland-shaders {};
-  illogical-impulse-hyprland-hyprland = pkgs.callPackage ./illogical-impulse-hyprland-hyprland {};
   illogical-impulse-kvantum = pkgs.callPackage ./illogical-impulse-kvantum {};
   illogical-impulse-oneui4-icons = pkgs.callPackage ./illogical-impulse-oneui4-icons {};
 })
