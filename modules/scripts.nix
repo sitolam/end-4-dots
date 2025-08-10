792174867feb21112ddfcc2e2f0e6452c1e80f5a
sitolam <otis.lammertyn@gmail.com>
2025-08-10 16:19:23 +0200

    fix: remove spotify script from downstream repo

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -34,38 +34,6 @@ let
     rm "tmp.png"
     '';
 
-    spotify-cava = pkgs.writeShellScriptBin "spotify-cava" ''
-      if [ $# -eq 0 ] ; then
-          echo "usage: ./spotify.sh <spotify-title>"
-          exit 1
-      else
-          spotifyTitle="$1"
-      fi
-
-
-      hyprctl dispatch focusworkspaceoncurrentmonitor 10
-
-      if [ -z "$(hyprctl clients | grep "$spotifyTitle" | grep -v "grep" | grep -v "./spotify.sh")" ]; then
-          hyprctl dispatch movecursor 0 0 
-
-          kitty --title "spotify cava" sh -c "exit"
-          kitty --title "spotify cava" sh -c "cava" &
-          sleep 0.5
-          hyprctl --batch "dispatch focuswindow 'initialtitle:(spotify cava)' ; dispatch togglefloating ; dispatch resizeactive exact 50% 55% ;  dispatch centerwindow 1 ; dispatch moveactive 70% 60%"
-          
-
-          spotify &
-          sleep 1
-          until [ ! -z "$(hyprctl clients | grep "$spotifyTitle" | grep -v "grep" | grep -v "./spotify.sh")" ] 
-          do
-          echo "" 
-          done
-          hyprctl --batch "dispatch focuswindow "initialtitle:$spotifyTitle" ; dispatch togglefloating ; dispatch resizeactive exact 50% 55% ; dispatch centerwindow 1 ; dispatch moveactive -70% -60%" 
-          sleep 3
-          playerctl play-pause
-      fi
-    '';
-
     update = pkgs.writeShellScriptBin "update" ''
       env NH_FLAKE="$NH_FLAKE"
       nh os switch
@@ -82,7 +50,6 @@ in
 
   home.packages = with pkgs; [
     screenshot-ocr
-    spotify-cava
     update
   ];
 }
